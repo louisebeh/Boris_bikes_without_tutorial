c4895107dc6453e4e3025559057c3e4c160efd3d
Louise Beh <louise.beh@gmail.com>
2015-02-04 20:14:48 +0000

    Added feature: Docking Station can list availble bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -24,4 +24,13 @@ class DockingStation
     bike_count == @capacity
   end
 
+  def available_bikes
+    @bikes.reject {|bike| bike.broken? }
+    #@bikes : denotes bikes within an array
+    #.reject : deletes elements from the forthcoming block that evaluate to true.
+    #{|bike| bike.broken? } : is an array of all broken bikes. r
+    # @bikes.reject {|bike| bike.broken? } deletes broken bikes from the @bikes array thus leaving the available bikes.
+  end
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-  let(:bike){Bike.new}
+
+  let(:bike){double :Bike, {broken?: false}}
+  let(:broken_bike){double :Bike, {broken?: true}}
   let(:station){DockingStation.new(capacity: 20)}
+
+    def fill_station(station)
+      20.times { station.dock(double :Bike) }
+    end
+
 
   it 'should accept a bike' do
     expect{station.dock(bike)}.to change{station.bike_count}.by 1
@@ -20,9 +27,15 @@ describe DockingStation do
   end
 
   it 'should not accept a bike when it\'s full' do
-    20.times {station.dock(bike)}
-    expect{station.dock(bike)}.to raise_error 'Station is full'
+    fill_station station
+    expect{ station.dock(bike) }.to raise_error(RuntimeError, 'Station is full')
     #Lambda: allows us to save a block of code as a variable
+  end
+
+  it 'should provide a list of available bikes' do #we now need to think about bike & DockingStation simultaneously
+    station.dock(bike)
+    station.dock(broken_bike)
+    expect(station.available_bikes).to eq([bike])
   end
 
 
